6c468df18d4bf7391226e3c93f98d35011e3dc28
linye3929 <linye3929@gmail.com>
2026-08-03 17:08:40 +0800

    chore(android): update application package name

--- im-client/packages/app-mobile/android/app/src/main/java/com/rayim/eureka32/app/MainActivity.kt
+++ im-client/packages/app-mobile/android/app/src/main/java/com/rayim/eureka32/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.appmobile
+package com.rayim.eureka32.app
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
--- im-client/packages/app-mobile/android/app/src/main/java/com/rayim/eureka32/app/MainApplication.kt
+++ im-client/packages/app-mobile/android/app/src/main/java/com/rayim/eureka32/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.appmobile
+package com.rayim.eureka32.app
 
 import android.app.Application
 import com.facebook.react.PackageList
